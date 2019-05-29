c2b583a0bf4b9b56111d8d7dc0e728b24f192077
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-29 15:18:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+favorite_sports = ["football", "basketball", "soccer", "flag"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(favorite_sports, surfing)
+favorite_sports << "arrays!"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(favorite_sports, crossfit)
+favorite_sports.unshift "wow"
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(favorite_sports)
+favorite_sports.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(favorite_sports)
+favorite_sports.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(favorite_sports[0])
+favorite_sports[0]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(favorite_sports)
+favorite_sports[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(favorite_sports)
+favorite_sports[3]
 end
